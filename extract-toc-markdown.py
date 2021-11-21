7d355434218e4bdd5e6d60a681d1d3fa7eb48461
Yoan Mollard <ymollard@users.noreply.github.com>
2021-11-21 16:29:37 +0100

    Count and link slides for Marp

--- extract-toc-markdown.py
+++ extract-toc-markdown.py
@@ -9,11 +9,15 @@ parser.add_argument("--spacedtitles",
                     default="count",
                     help="How to treat titles with >= 2 initial spaces after #")
 parser.add_argument("--max-depth", type=int, default=10, help="Ignore titles behind this depth")
+parser.add_argument("--link-slides-level", type=int, default=-1, help="Generate Mardown links to slides for each title of this level or below.")
+parser.add_argument("--slides-separator", type=str, default="---", help="String that separates slides from each other")
+
 args = parser.parse_args()
 
 hold_verbatim = False
 levels = [0]
 previous_section_line_num = 1
+slide_num = 0
 
 def _count_trailing_spaces(string: str) -> int:
     i = 0
@@ -46,7 +50,15 @@ with open(args.path) as f:
                 must_print_spacedtitles = initial_spaces < 2 or args.spacedtitles != "ignore"
                 must_print_depth = level <= args.max_depth
                 if must_print_spacedtitles and must_print_depth:
-                    print(level_string + ". " + line.strip().replace("#", ""))
+                    title_str = line.strip().replace("#", "")
+
+                    if args.link_slides_level >= level:
+                        title_str = "[" + title_str + "](#" + str(slide_num-1) + ")"
+
+                    print(level_string + ". " + title_str)
                     previous_section_line_num = 0
+
+            elif line.startswith(args.slides_separator):
+                slide_num += 1
             else:
                 previous_section_line_num += 1
